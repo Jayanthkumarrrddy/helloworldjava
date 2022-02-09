5826cb34a2e7c56c5591db4f3b465354aeb5c82f
D Jayanth Kumar Reddy <67574710+Jayanthkumarrrddy@users.noreply.github.com>
2022-02-09 07:59:45 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
    public static void main(String[] args) {
-    System.out.println("Hello India, triggered by webhook check1!");
+    System.out.println("Hello India, triggered by webhook check2!");
     }
 }
